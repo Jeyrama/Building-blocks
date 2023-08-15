3058d0d5a33525ba9148d1a6c7d5d615dcd5f9aa
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-15 12:06:26 -0700

    Update Building-blocks.js

--- Building-blocks.js
+++ Building-blocks.js
@@ -21,3 +21,20 @@ a width of `2` a length of `4` and a height of `6`:
 
 
 // Solution
+
+class Block {
+  constructor(data){
+    [this.w, this.l, this.h] = data;
+  }
+  getWidth(){ return this.w }
+  getLength(){ return this.l }
+  getHeight(){ return this.h }
+  getVolume(){
+    let {l, w, h} = this;
+    return w*l*h;
+  }
+  getSurfaceArea(){
+    let {l, w, h} = this;
+    return 2 * (l*w + l*h + w*h);
+  }
+}
